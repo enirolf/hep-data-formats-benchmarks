dbbc3a4664fcc91d4633b84b3a90cbf56e3f15f5
Florine de Geus <florine.de.geus@cern.ch>
2025-07-24 11:40:51 +0200

    Add option for uncompressed conversion

--- convert.py
+++ convert.py
@@ -15,34 +15,36 @@ def read_as_ak(dataset_name, dataset_path):
     return ak_array
 
 
-def convert2orc(ak_array, output_name):
+def convert2orc(ak_array, output_name, uncompressed):
+    compression = "UNCOMPRESSED" if uncompressed else "ZSTD"
     arrow_table = ak.to_arrow_table(ak_array, extensionarray=False)
     orc.write_table(
         arrow_table,
         output_name,
-        compression="ZSTD",
+        compression=compression,
         compression_block_size=1024 * 1024,
         stripe_size=1024 * 1024 * 128,
     )
 
 
-def convert2parquet(ak_array, output_name):
+def convert2parquet(ak_array, output_name, uncompressed):
+    compression = "NONE" if uncompressed else "ZSTD"
     ak.to_parquet(
         ak_array,
         output_name,
         extensionarray=False,
-        compression="ZSTD",
-        compression_level=3,
+        compression="NONE" if uncompressed else "ZSTD",
+        compression_level=None if uncompressed else 3,
         parquet_metadata_statistics=False,
         data_page_size=1024 * 1024,
     )
 
 
-def convert2rntuple(dataset_name, input_path, output_path):
+def convert2rntuple(dataset_name, input_path, output_path, uncompressed):
     rdf = ROOT.RDataFrame(dataset_name, input_path)
     opts = ROOT.RDF.RSnapshotOptions()
-    opts.fCompressionAlgorithm = ROOT.RCompressionSetting.EAlgorithm.kZSTD
-    opts.fCompressionLevel = 1
+    opts.fCompressionAlgorithm = ROOT.RCompressionSetting.EAlgorithm.kUndefined if uncompressed else ROOT.RCompressionSetting.EAlgorithm.kZSTD
+    opts.fCompressionLevel = 0 if uncompressed else 1
     opts.fOutputFormat = ROOT.RDF.ESnapshotOutputFormat.kRNTuple
     opts.fOverwriteIfExists = True
     rdf.Snapshot(dataset_name, output_path, "", opts)
@@ -56,21 +58,28 @@ if __name__ == "__main__":
     parser.add_argument("input_path", help="path to the input RNTuple")
     parser.add_argument("output_path", help="path for the output file")
     parser.add_argument(
-        "-o, --output_mode",
+        "-o", "--output_mode",
         dest="output_mode",
         choices=["orc", "parquet", "rntuple"],
         default="orc",
         help="format to convert to",
     )
+    parser.add_argument(
+        "-u",
+        "--uncompressed",
+        dest="uncompressed",
+        action="store_true",
+        help="don't compress the data",
+    )
 
     args = parser.parse_args()
 
     if args.output_mode == "rntuple":
-        convert2rntuple(args.dataset_name, args.input_path, args.output_path)
+        convert2rntuple(args.dataset_name, args.input_path, args.output_path, args.uncompressed)
         exit(0)
 
     ak_array = read_as_ak(args.dataset_name, args.input_path)
     if args.output_mode == "orc":
-        convert2orc(ak_array, args.output_path)
+        convert2orc(ak_array, args.output_path, args.uncompressed)
     if args.output_mode == "parquet":
-        convert2parquet(ak_array, args.output_path)
+        convert2parquet(ak_array, args.output_path, args.uncompressed)
