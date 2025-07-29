065764dd7eb1ef85a729c0dc5220560ffba63ab4
Florine de Geus <florine.de.geus@cern.ch>
2025-07-29 12:26:32 +0200

    Add direct RNTuple/Parquet reading

--- cms.cxx
+++ cms.cxx
@@ -1,5 +1,6 @@
 #include <ROOT/RArrowDS.hxx>
 #include <ROOT/RDataFrame.hxx>
+#include <ROOT/RNTupleReader.hxx>
 
 #include <TCanvas.h>
 #include <TH1D.h>
@@ -7,13 +8,199 @@
 #include <TStyle.h>
 #include <TSystem.h>
 
+#include <arrow/io/api.h>
+#include <parquet/arrow/reader.h>
+
 #include <chrono>
 #include <iostream>
 #include <string>
 
 #include "util.hxx"
 
-static AnalysisTime_t analysis(ROOT::RDataFrame &df, const std::string &histo_path) {
+static AnalysisTime_t analysis_parquet(const std::string &path,
+                                       const std::string &histo_path) {
+  auto ts_init = std::chrono::steady_clock::now();
+
+  auto hMass = std::make_unique<TH1D>("Dimuon_mass", "Dimuon_mass", 2000, 0.25, 300);
+
+  arrow::Status st;
+  arrow::MemoryPool *pool = arrow::default_memory_pool();
+
+  parquet::arrow::FileReaderBuilder reader_builder;
+  st = reader_builder.OpenFile(path);
+  if (!st.ok()) {
+    throw std::runtime_error("could not create reader builder");
+  }
+  reader_builder.memory_pool(pool);
+
+  auto reader = reader_builder.Build().ValueOrDie();
+  reader->set_use_threads(false);
+  auto n_row_groups = reader->num_row_groups();
+  std::shared_ptr<arrow::Table> table;
+
+  std::shared_ptr<arrow::Schema> schema;
+  st = reader->GetSchema(&schema);
+  if (!st.ok()) {
+    throw std::runtime_error("could not get schema");
+  }
+
+  std::vector<std::int32_t> columns;
+  columns.emplace_back(schema->GetFieldIndex("nMuon"));
+  columns.emplace_back(schema->GetFieldIndex("Muon_charge"));
+  columns.emplace_back(schema->GetFieldIndex("Muon_pt"));
+  columns.emplace_back(schema->GetFieldIndex("Muon_eta"));
+  columns.emplace_back(schema->GetFieldIndex("Muon_phi"));
+  columns.emplace_back(schema->GetFieldIndex("Muon_mass"));
+
+  std::chrono::steady_clock::time_point ts_first =
+      std::chrono::steady_clock::now();
+
+  for (std::int32_t row_group = 0; row_group < n_row_groups; ++row_group) {
+    auto st = reader->ReadRowGroup(row_group, columns, &table);
+    assert(st.ok());
+
+    assert(table->GetColumnByName("nMuon")->num_chunks() == 1);
+    assert(table->GetColumnByName("Muon_charge")->num_chunks() == 1);
+    assert(table->GetColumnByName("Muon_pt")->num_chunks() == 1);
+    assert(table->GetColumnByName("Muon_eta")->num_chunks() == 1);
+    assert(table->GetColumnByName("Muon_phi")->num_chunks() == 1);
+    assert(table->GetColumnByName("Muon_mass")->num_chunks() == 1);
+
+    auto nMuonArr = std::static_pointer_cast<arrow::Int32Array>(table->GetColumnByName("nMuon")->chunk(0));
+    auto muonChargeArr = std::static_pointer_cast<arrow::ListArray>(table->GetColumnByName("Muon_charge")->chunk(0));
+    auto muonPtArr = std::static_pointer_cast<arrow::ListArray>(table->GetColumnByName("Muon_pt")->chunk(0));
+    auto muonEtaArr = std::static_pointer_cast<arrow::ListArray>(table->GetColumnByName("Muon_eta")->chunk(0));
+    auto muonPhiArr = std::static_pointer_cast<arrow::ListArray>(table->GetColumnByName("Muon_phi")->chunk(0));
+    auto muonMassArr = std::static_pointer_cast<arrow::ListArray>(table->GetColumnByName("Muon_mass")->chunk(0));
+
+    std::shared_ptr<arrow::Int32Array> nMuonsArray;
+    auto rawNMuonVals = nMuonArr->raw_values();
+    ROOT::RVec<std::int32_t> nMuons(rawNMuonVals, rawNMuonVals + nMuonArr->length());
+
+    ROOT::RVec<std::int32_t> muonCharge;
+    ROOT::RVec<float> muonPt, muonEta, muonPhi, muonMass;
+
+
+    for (int64_t entryId = 0; entryId < table->num_rows(); ++entryId) {
+      if (nMuons[entryId] != 2)
+        continue;
+
+      fill_vector_from_arrow(entryId, *muonChargeArr, muonCharge);
+
+      if (muonCharge[0] == muonCharge[1]) {
+        continue;
+      }
+
+      fill_vector_from_arrow(entryId, *muonPtArr, muonPt);
+      fill_vector_from_arrow(entryId, *muonEtaArr, muonEta);
+      fill_vector_from_arrow(entryId, *muonPhiArr, muonPhi);
+      fill_vector_from_arrow(entryId, *muonMassArr, muonMass);
+
+      float x_sum = 0.;
+      float y_sum = 0.;
+      float z_sum = 0.;
+      float e_sum = 0.;
+      for (std::size_t i = 0u; i < 2; ++i) {
+        // Convert to (e, x, y, z) coordinate system and update sums
+        const auto x = muonPt[i] * std::cos(muonPhi[i]);
+        x_sum += x;
+        const auto y = muonPt[i] * std::sin(muonPhi[i]);
+        y_sum += y;
+        const auto z = muonPt[i] * std::sinh(muonEta[i]);
+        z_sum += z;
+        const auto e = std::sqrt(x * x + y * y + z * z + muonMass[i] * muonMass[i]);
+        e_sum += e;
+      }
+      // Return invariant mass with (+, -, -, -) metric
+      auto fmass = std::sqrt(e_sum * e_sum - x_sum * x_sum - y_sum * y_sum -
+                             z_sum * z_sum);
+      hMass->Fill(fmass);
+    }
+  }
+
+  auto ts_end = std::chrono::steady_clock::now();
+  auto runtime_init =
+      std::chrono::duration_cast<std::chrono::microseconds>(ts_first - ts_init)
+          .count();
+  auto runtime_analyze =
+      std::chrono::duration_cast<std::chrono::microseconds>(ts_end - ts_first)
+          .count();
+
+  if (!histo_path.empty())
+    save_histogram(hMass.get(), histo_path);
+
+  return std::make_pair(runtime_init, runtime_analyze);
+}
+
+static AnalysisTime_t analysis_rntuple(std::string_view ntuple_path,
+                                       const std::string &histo_path) {
+  auto ts_init = std::chrono::steady_clock::now();
+
+  auto ntuple =
+      ROOT::RNTupleReader::Open("Events", ntuple_path);
+
+  auto hMass = std::make_unique<TH1D>("Dimuon_mass", "Dimuon_mass", 2000, 0.25, 300);
+
+  auto viewNMuon = ntuple->GetView<std::int32_t>("nMuon");
+  auto viewMuonCharge = ntuple->GetView<ROOT::RVec<std::int32_t>>("Muon_charge");
+  auto viewMuonPt = ntuple->GetView<ROOT::RVec<float>>("Muon_pt");
+  auto viewMuonEta = ntuple->GetView<ROOT::RVec<float>>("Muon_eta");
+  auto viewMuonPhi = ntuple->GetView<ROOT::RVec<float>>("Muon_phi");
+  auto viewMuonMass = ntuple->GetView<ROOT::RVec<float>>("Muon_mass");
+
+  std::chrono::steady_clock::time_point ts_first =
+      std::chrono::steady_clock::now();
+
+  for (auto entryId : ntuple->GetEntryRange()) {
+    if (viewNMuon(entryId) != 2)
+      continue;
+
+    auto charges = viewMuonCharge(entryId);
+    if (charges[0] == charges[1])
+      continue;
+
+    auto pt = viewMuonPt(entryId);
+    auto eta = viewMuonEta(entryId);
+    auto phi = viewMuonPhi(entryId);
+    auto mass = viewMuonMass(entryId);
+
+    float x_sum = 0.;
+    float y_sum = 0.;
+    float z_sum = 0.;
+    float e_sum = 0.;
+    for (std::size_t i = 0u; i < 2; ++i) {
+      // Convert to (e, x, y, z) coordinate system and update sums
+      const auto x = pt[i] * std::cos(phi[i]);
+      x_sum += x;
+      const auto y = pt[i] * std::sin(phi[i]);
+      y_sum += y;
+      const auto z = pt[i] * std::sinh(eta[i]);
+      z_sum += z;
+      const auto e = std::sqrt(x * x + y * y + z * z + mass[i] * mass[i]);
+      e_sum += e;
+    }
+    // Return invariant mass with (+, -, -, -) metric
+    auto fmass = std::sqrt(e_sum * e_sum - x_sum * x_sum - y_sum * y_sum -
+                           z_sum * z_sum);
+    hMass->Fill(fmass);
+  }
+
+  auto ts_end = std::chrono::steady_clock::now();
+  auto runtime_init =
+      std::chrono::duration_cast<std::chrono::microseconds>(ts_first - ts_init)
+          .count();
+  auto runtime_analyze =
+      std::chrono::duration_cast<std::chrono::microseconds>(ts_end - ts_first)
+          .count();
+
+  if (!histo_path.empty())
+    save_histogram(hMass.get(), histo_path);
+
+  return std::make_pair(runtime_init, runtime_analyze);
+}
+
+static AnalysisTime_t analysis_rdf(ROOT::RDataFrame &df,
+                                   const std::string &histo_path) {
   auto ts_init = std::chrono::steady_clock::now();
   std::chrono::steady_clock::time_point ts_first;
   bool ts_first_set = false;
@@ -73,15 +260,19 @@ int main(int argc, char **argv) {
   AnalysisTime_t runtime_analysis;
   switch (fmt) {
   case FileFormat::rntuple: {
-    ROOT::RDataFrame df("Events", input_path);
-    runtime_analysis = analysis(df, histo_path);
+    runtime_analysis = analysis_rntuple(input_path, histo_path);
   } break;
-  case FileFormat::orc:
   case FileFormat::parquet: {
+    runtime_analysis = analysis_parquet(input_path, histo_path);
+    break;
+  }
+  case FileFormat::orc: {
     std::shared_ptr<arrow::Table> table = open_arrow(input_path, fmt);
     auto colNames = get_column_names(basename);
     auto df = ROOT::RDF::FromArrow(table, colNames);
-    runtime_analysis = analysis(df, histo_path);
+    auto desc = df.Describe();
+    desc.Print();
+    runtime_analysis = analysis_rdf(df, histo_path);
   } break;
   default:
     std::cerr << "Invalid file format: " << suffix << std::endl;
@@ -93,7 +284,8 @@ int main(int argc, char **argv) {
       std::chrono::duration_cast<std::chrono::microseconds>(ts_end - ts_init)
           .count();
 
-  std::cout << runtime_analysis.first << ", " << runtime_analysis.second << ", " << runtime_main << std::endl;
+  std::cout << runtime_analysis.first << ", " << runtime_analysis.second << ", "
+            << runtime_main << std::endl;
 
   return 0;
 }
--- util.hxx
+++ util.hxx
@@ -1,9 +1,13 @@
 #ifndef UTIL__HXX
 #define UTIL__HXX
 
+#include <ROOT/RVec.hxx>
+#include <arrow/io/api.h>
+
 #include <string>
 #include <vector>
 #include <memory>
+#include <iostream>
 
 #include <TH1D.h>
 
@@ -25,4 +29,58 @@ std::shared_ptr<arrow::Table> open_arrow(const std::string &input_path, FileForm
 
 void save_histogram(TH1D *hist, const std::string &output_path);
 
+template <typename T>
+struct RootConversionTraits {};
+
+#define ROOT_ARROW_STL_CONVERSION(c_type, ArrowType_)  \
+   template <>                                         \
+   struct RootConversionTraits<c_type> {               \
+   using ArrowType = ::arrow::ArrowType_;              \
+   };
+
+ROOT_ARROW_STL_CONVERSION(bool, BooleanType)
+ROOT_ARROW_STL_CONVERSION(std::int8_t, Int8Type)
+ROOT_ARROW_STL_CONVERSION(std::int16_t, Int16Type)
+ROOT_ARROW_STL_CONVERSION(std::int32_t, Int32Type)
+ROOT_ARROW_STL_CONVERSION(std::int64_t, Int64Type)
+ROOT_ARROW_STL_CONVERSION(std::uint8_t, UInt8Type)
+ROOT_ARROW_STL_CONVERSION(std::uint16_t, UInt16Type)
+ROOT_ARROW_STL_CONVERSION(std::uint32_t, UInt32Type)
+ROOT_ARROW_STL_CONVERSION(std::uint64_t, UInt64Type)
+ROOT_ARROW_STL_CONVERSION(float, FloatType)
+ROOT_ARROW_STL_CONVERSION(double, DoubleType)
+ROOT_ARROW_STL_CONVERSION(std::string, StringType)
+
+template <typename T>
+void fill_vector_from_arrow(std::int32_t entryId, const arrow::ListArray &src, ROOT::RVec<T> &dest) {
+  using ArrowType = typename RootConversionTraits<T>::ArrowType;
+  using ArrayType = typename arrow::TypeTraits<ArrowType>::ArrayType;
+
+  auto entrySlice = std::static_pointer_cast<arrow::ListArray>(src.Slice(entryId, 1));
+  auto entryArray = std::static_pointer_cast<ArrayType>(entrySlice->value_slice(0));
+  ROOT::RVec<T> tmp(reinterpret_cast<T *>((void *)entryArray->raw_values()), entryArray->length());
+  std::swap(tmp, dest);
+}
+
+template <typename T>
+void print_vec(const ROOT::RVec<T> &vec) {
+  std::cout << "{ ";
+  for (unsigned i = 0; i < vec.size(); ++i) {
+    std::cout << vec[i];
+    if (i < vec.size() - 1)
+      std::cout << ", ";
+  }
+  std::cout << " }" << std::endl;
+}
+
+template <typename T>
+void print_vec(const std::vector<T> &vec) {
+  std::cout << "{ ";
+  for (unsigned i = 0; i < vec.size(); ++i) {
+    std::cout << vec[i];
+    if (i < vec.size() - 1)
+      std::cout << ", ";
+  }
+  std::cout << " }" << std::endl;
+}
 #endif // UTIL__HXX
